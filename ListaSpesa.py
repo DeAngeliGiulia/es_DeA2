ed839755b855d3a8c7010a9cd7448f510a99a57b
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-11-14 07:59:50 +0000

    inizio fase 3 punto 1

--- ListaSpesa.py
+++ ListaSpesa.py
@@ -12,9 +12,15 @@ def elimina():
     id = int(input("Inserisci l'id del prodotto da eliminare: "))
     lista.pop(id - 1)
 
+def conta():
+    print(len(lista))
+    
+
 while True:
-    scelta = int(input("1. Aggiungi elemento\n2. Elimina elemento\n3. Visualizza lista\n4. Esci "))
-    if scelta == 1:
+    scelta = int(input("0. Esci\n1. Aggiungi elemento\n2. Elimina elemento\n3. Visualizza lista\n4. Conta elementi "))
+    if scelta == 0:
+        break
+    elif scelta == 1:
         aggiungi()
     elif scelta == 2:
         elimina()
