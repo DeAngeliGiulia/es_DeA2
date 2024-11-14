d2f9d2bdf45129b3504979a6db4c98d0184cbf8f
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-11-14 07:31:59 +0000

    2

--- ListaSpesa.py
+++ ListaSpesa.py
@@ -1,1 +1,8 @@
-lista = [""]
+lista = []
+def aggiungi():
+    elementi = int(input("Quanti elementi vuoi aggiungere alla lista? "))
+    for i in range(elementi):
+        prodotto = input("Inserisci prodotto: ")
+        lista.append(prodotto)
+    print(lista)
+aggiungi()
