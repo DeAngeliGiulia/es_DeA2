a27449777e3c00ec85ad685b63748b69da9c984c
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-11-14 07:54:41 +0000

    5

--- ListaSpesa.py
+++ ListaSpesa.py
@@ -7,6 +7,20 @@ def aggiungi():
 
 def visualizza():
     print(lista)
-    
-aggiungi()
-visualizza()
+
+def elimina():
+    id = int(input("Inserisci l'id del prodotto da eliminare: "))
+    lista.pop(id - 1)
+
+while True:
+    scelta = int(input("1. Aggiungi elemento\n2. Elimina elemento\n3. Visualizza lista\n4. Esci "))
+    if scelta == 1:
+        aggiungi()
+    elif scelta == 2:
+        elimina()
+    elif scelta == 3:
+        visualizza()
+    elif scelta == 4:
+        break
+    else:
+        print("Opzione non valida")
