db565e71a0e956a58ae8d8e2a195723b9dc85291
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-11-19 08:16:06 +0000

    fase 2

--- ListaSpesa2/app.py
+++ ListaSpesa2/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Flask, render_template
 
 #inizializza l'app Flask
 app = Flask(__name__)
@@ -6,8 +7,8 @@ app = Flask(__name__)
 #rotta principale
 @app.route('/')
 def home():
-    return "Per ora funziona tutto"
-    
+    return render_template('index.html')
+
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
