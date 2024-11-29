185bff9df88471fad876a8d1131a178cff8e723b
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-11-29 12:57:16 +0000

    da finire il css

--- ListaSpesa2/app.py
+++ ListaSpesa2/app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from models import db, ListaSpesa
 
 # Inizializza l'app Flask
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 lista_spesa = []
 
 # Route principale
 @app.route('/')
 def home():
+    lista_spesa = ListaSpesa.query.all()
     return render_template('index.html', lista_spesa=lista_spesa)
 
 # Route per aggiungere un elemento
@@ -15,21 +26,23 @@ def home():
 def aggiungi():
     elemento = request.form.get('elemento', '').strip()
     if elemento:
-        lista_spesa.append(elemento)
-    else:
-        print("Errore: Elemento vuoto non aggiunto.")
+        nuovo_elemento = ListaSpesa(elemento=elemento)
+        db.session.add(nuovo_elemento)
+        db.session.commit()
     return redirect(url_for('home'))
 
 # Route per rimuovere un elemento
 @app.route('/rimuovi/<int:indice>', methods=['POST'])
 def rimuovi(indice):
-    if 0 <= indice < len(lista_spesa):
-        lista_spesa.pop(indice) 
+    elemento = ListaSpesa.query.get_or_404(indice)
+    db.session.delete(elemento)
+    db.session.commit()
     return redirect(url_for('home'))
 
 @app.route('/svuota', methods=['POST'])
 def svuota_lista():
-    lista_spesa.clear()
+    ListaSpesa.query.delete()
+    db.session.commit()
     return redirect(url_for('home'))
 
 # Avvio dell'app Flask
